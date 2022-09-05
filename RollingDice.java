a5b99328ef7b621d274fb4553ad7bbac1023cea9
LukeMel <50152526+LukeMel@users.noreply.github.com>
2022-09-05 13:43:37 +0200

    Update RollingDice.java

--- RollingDice.java
+++ RollingDice.java
@@ -4,11 +4,17 @@ import java.util.Random;
 public class RollingDice {
 
 	public static void main(String[] args) {
+		
+				//  with math.random
+		//  int max = 6;
+		//  int min = 1;
+		//  int range = max - min + 1;
+		//  int result = (int)(Math.random() * range) + min;
 
+				 //  with utilRandom
 		Random rand = new Random();
 		int result = rand.nextInt(6) + 1;
 		System.out.println("You rolled a: " + result);
-
 	}
 
 }
